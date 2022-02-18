5014cbd4f2dbc4231f915acc2487e649c10edfb6
Tsukasa-08 <takahashi.299792458@gmail.com>
2022-02-18 17:21:57 +0900

    tomlファイルから[Efield_0]を除去
    のちのち必要になりそうだったら考える

--- tools/goodtools/make_toml_Efield_0.sh
+++ tools/goodtools/make_toml_Efield_0.sh
@@ -3,7 +3,7 @@
 find . -name INPUT | head -n 1 | xargs -I{} cp {} .
 find . -name OUTPUT | head -n 1 | xargs -I{} cp {} .
 exec >> Efield_0_parameter.toml
-echo "[Efield_0]"
+#echo "[Efield_0]"
 echo -n "temperture = " ; cat INPUT OUTPUT | grep TEMP | sed -e 's/[^0-9]//g'
 echo -n "concentration = " ; cat INPUT OUTPUT | grep concentration | awk '{print $4}'
 echo -n "total_time = " ; cat INPUT OUTPUT | grep total_time | awk '{print $4}'
