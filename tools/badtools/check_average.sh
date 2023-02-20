65aa1e9b0e4bc60e2d43153e8001a1e0c3a761f0
Tsukasa-08 <takahashi.299792458@gmail.com>
2023-02-20 12:10:48 +0900

    不要なtoolsの削除

--- tools/badtools/check_average.sh
+++
@@ -1,1 +0,0 @@
-awk -F"," '{print $5}' mean_displacement.csv | sed '/^$/d' | awk -f ~/awk_file/standard_deviation.awk
--- tools/badtools/make_sum_ini.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-#4つのiniファイルを1つにまとめる
-for i in for_sigma_calc_E* ; do cat $i ; echo "" ; done > for_sigma_calc_sum.ini
